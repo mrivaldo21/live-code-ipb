338936ede350580603c85b486f528407fc0456ec
MUHAMMAD RIVALDO <Muhammadrivaldo39@gmail.com>
2023-05-28 13:38:40 +0700

    reset list item style

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,10 @@ function App() {
       <button onClick={() => addNewTodo()}>Create</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <li key={todo.id} className="todo-item">
+            <input type="checkbox" />
+            {todo.title}
+          </li>
         ))}
       </ul>
     </>

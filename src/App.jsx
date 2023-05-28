7d991af180a2db7f19cac9de49ad8dca2e6f1c77
MUHAMMAD RIVALDO <Muhammadrivaldo39@gmail.com>
2023-05-28 14:02:40 +0700

    create strikethrough

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,16 @@ function App() {
     setNewTodo("");
   }
 
+  function completeTodo(targetTodoId) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === targetTodoId) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
+
   return (
     <>
       <h1>ToDo App</h1>
@@ -39,8 +49,14 @@ function App() {
       <button onClick={() => addNewTodo()}>Create</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id} className="todo-item">
-            <input type="checkbox" />
+          <li
+            key={todo.id}
+            className="todo-item"
+            style={{
+              textDecoration: todo.isCompleted ? "line-through" : "none",
+            }}
+          >
+            <input type="checkbox" onChange={() => completeTodo(todo.id)} />
             {todo.title}
           </li>
         ))}
